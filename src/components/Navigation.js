c903e0c0a9b8b6eac1f9d85bce6aee0009a4261e
Zolon Wilkins <zoloniv@gmail.com>
2020-01-18 10:29:06 -0800

    hack to get the drawer open

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,20 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { NavLink } from 'react-router-dom'
 import NavigationDrawer from './navigation/NavigationDrawer';
+import { withRouter } from 'react-router';
 
-function Navigation({ match }) {
-
+function Navigation(props) {
   const [activeIndex, setActiveIndex] = useState(undefined)
+
+  useEffect(() => {
+    let path = props.location.pathname
+    let index
+    switch (path) {
+      case "/home":
+      case "/dashboard":
+      case "/blog":
+        index = 0
+        break;
+      case "/news":
+      case "/contact":
+      case "/about":
+        index = 1
+        break;
+      default:
+        index = undefined
+    }
+    setActiveIndex(index)
+  }, [props.location]);
 
   return (
     <div className="h-screen w-56 bg-gray-200">
       <div className="flex flex-col p-6">
-        <NavigationDrawer title="General" index="0" activeDrawerIndex={activeIndex} setActiveIndex={setActiveIndex}>
+        <NavigationDrawer title="General" index={0} activeDrawerIndex={activeIndex} setActiveIndex={setActiveIndex}>
           <NavLink to="/home" className="hover:underline" activeClassName="font-bold">Home</NavLink>
           <NavLink to="/dashboard" className="hover:underline" activeClassName="font-bold">Dashboard</NavLink>
           <NavLink to="/blog" className="hover:underline" activeClassName="font-bold">Blog</NavLink>
         </NavigationDrawer>
-        <NavigationDrawer title="Special" index="1" activeDrawerIndex={activeIndex} setActiveIndex={setActiveIndex}>
+        <NavigationDrawer title="Special" index={1} activeDrawerIndex={activeIndex} setActiveIndex={setActiveIndex}>
           <NavLink to="/news" className="hover:underline" activeClassName="font-bold">News</NavLink>
           <NavLink to="/contact" className="hover:underline" activeClassName="font-bold">Contact</NavLink>
           <NavLink to="/about" className="hover:underline" activeClassName="font-bold">About</NavLink>
@@ -23,4 +43,6 @@ function Navigation({ match }) {
     </div>
   )
 }
-export default Navigation
+
+
+export default withRouter(Navigation);
--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -5,7 +5,7 @@ function Landing() {
   return (
     <div className="px-4 py-6 w-full h-screen flex flex-col items-center justify-center">
       <h1 className="text-5xl font-bold">Hello!</h1>
-      <span>Start by going to the <Link to="/Home" className="underline text-blue-700 wavy">Home</Link> page</span>
+      <span>Start by going to the <Link to="/home" className="underline text-blue-700 wavy">Home</Link> page</span>
     </div>
   )
 }
